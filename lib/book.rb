f822cf58c66cb44cd6aa2181fcb0e63bf7016f71
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-26 23:05:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,14 +9,26 @@ class Book
   end
 
   def author
+    @author
   end
 
   def author=(author)
+    @author = author
   end
 
-  def page_count=()
+  def page_count
+    @num
+  end
+
+  def page_count=(num)
+    @num = num
   end
 
   def genre
+    @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
   end
 end
